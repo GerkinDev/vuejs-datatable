f7bb88c0913a4ba5a23f8d0120cae981f69b85c0
Patrick Stephan <pstephan1187@gmail.com>
2017-08-04 11:53:37 -0500

    Removed old examples.

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,6 @@ let mix = require('laravel-mix');
  */
 
 mix.js('src/es5.js', 'dist/vuejs-datatable.js')
-   .js('examples/vue-2/app.js', 'examples/vue-2/build/app.js')
    .js('tests/src/index.js', 'tests/spec/index.js')
    .js('examples/ajax/app.js', 'examples/ajax/build/app.js');
 
