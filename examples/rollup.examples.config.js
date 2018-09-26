7d5cdc9ff6081edf8ca5fa1eac2e5028f90cbec6
Gerkin <nihilivin@gmail.com>
2018-09-26 17:12:43 +0200

    fix(examples): Fixed undefined node globals

--- examples/rollup.examples.config.js
+++ examples/rollup.examples.config.js
@@ -1,13 +1,15 @@
-import { terser } from 'rollup-plugin-terser';
 import resolve from 'rollup-plugin-node-resolve';
+import globals from 'rollup-plugin-node-globals';
 import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
+import { terser } from 'rollup-plugin-terser';
 
 // The module name
 const name = 'vuejs-datatable';
 
 // Plugins used for build
 const plugins = [
+	resolve({ browser: true }),
 	commonjs({
 		namedExports: {
 			// left-hand side can be an absolute path, a path
@@ -17,8 +19,8 @@ const plugins = [
 			'axios': [ 'get' ],
 		}
 	}),
+	globals(),
 	json(),
-	resolve(),
 	terser(),
 ];
 
@@ -31,6 +33,7 @@ const sourcemap = true;
 const generateDemoConfig = dir => ({
 	input: `${__dirname}/${dir}/app.js`,
 	output: {
+		preferBuiltins: false,
 		file: `${__dirname}/${dir}/${outDir}/app.js`,
 		format: 'iife',
 		// Use `name` as window to hack a bit & avoid exports.
