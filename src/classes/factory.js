278ca6073e79688bdd7994028bdc6aebc015427a
Alexandre Germain <agermain@ithoughts.io>
2019-02-25 10:18:53 +0100

    refactor: 💡 Factory can now register & deregister table types
    Simplified register/deregister process, that may take a single
    parameter: the `TableType` instance to register. The default table type
    is now constant and can be overridden only by re-registering it.
    
    BREAKING CHANGE: Removed `Factory.defaultTableSettings`. The default table type settings
    can no more be customized at the factory-level

--- src/classes/factory.js
+++ src/classes/factory.js
@@ -2,12 +2,17 @@ import VueDatatableCell from '../vue-datatable-cell.vue';
 import VueDatatableHeader from '../vue-datatable-header.vue';
 import VueDatatablePagerButton from '../vue-datatable-pager-button.vue';
 import TableType from './table-type.js';
-import Settings from './settings.js';
 
+/**
+ * @member {'datatable'} DEFAULT_DATATABLE - The default table type name
+ * @memberof DatatableFactory
+ * @public
+ * @readonly
+ */
 const DEFAULT_DATATABLE = 'datatable';
+
 /**
  * Registers Vuejs-Datatable components globally in VueJS.
- * You should add table types to the {@link table_types} list before using `Vue.use`.
  * 
  * @example import DatatableFactory from 'vuejs-datatable';
 	Vue.use(DatatableFactory);
@@ -17,105 +22,143 @@ class DatatableFactory {
 	 * Initialize the default factory
 	 */
 	constructor(){
-		/** @member {Vue | undefined} - A reference to the Vue instance the plugin is installed in. */
-		this.vueConstructor = undefined;
-		/** @member {Dictionary<TableType>} - Registry of declared table types. */
+		/**
+		 * @private
+		 * @member {VueConstructor | undefined} - A reference to the Vue instance the plugin is installed in. It may be used to check if the factory was already installed
+		 */
+		this.vueInstance = undefined;
+		/**
+		 * @private
+		 * @member {Dictionary<TableType>} - Registry of declared table types.
+		 */
 		this.tableTypes = {};
-		/** @member {boolean} - Controls whetever the module should register a default table type automatically. */
-		this._useDefaultType = true;
-		/** @member {Settings} - Base settings instance to merge with custom table type settings. */
-		this.defaultTableSettings = new Settings();
+		/**
+		 * @private
+		 * @member {TableType} - The default table type to use if no other configuration was provided.
+		 */
+		this.defaultTableType = new TableType(DEFAULT_DATATABLE);
+		
+		this.useDefaultType(true);
+	}
+
+	/**
+	 * Get a table type by its identifier.
+	 * 
+	 * @param {string} [id = DEFAULT_DATATABLE] - The identifier of the table type. If not provided, it will default to the default table type.
+	 * @returns {TableType | undefined} The table type registered with that identifier.
+	 */
+	getTableType(id = DEFAULT_DATATABLE){
+		return this.tableTypes[id];
 	}
 
 	/**
 	 * Controls the definition of default table type.
 	 * 
-	 * @param {boolean} [use] - `true` to use the default type, false otherwise. If not provided, this method is a getter function
+	 * @param {boolean} [use] - `true` to use the default type, `false` otherwise. If not provided, this method returns a boolean indicating if the default table type is / will be used.
 	 * @returns {this | boolean} - `this` for chaining, or the value if `use` is undefined
 	 */
 	useDefaultType(use){
 		if (typeof use !== 'boolean' && !use){
-			return this._useDefaultType;
+			return this.tableTypes[DEFAULT_DATATABLE] === this.defaultTableType;
 		}
-		this._useDefaultType = !!use;
-
-		if (this.vueConstructor){
-			if (use){
-				this.installTableType(this.tableTypes[DEFAULT_DATATABLE].getId(), this.tableTypes[DEFAULT_DATATABLE], this.vueConstructor);
-			} else {
-				delete this.vueConstructor.options.components[DEFAULT_DATATABLE];
-				delete this.vueConstructor.options.components[`${ DEFAULT_DATATABLE }-pager`];
-			}
-			
+		if (use){
+			this.registerTableType(this.defaultTableType);
+		} else {
+			this.deregisterTableType(this.defaultTableType);
 		}
+		
 		return this;
 	}
 
 	/**
 	 * Creates a new table type with a specified prefix, that you can customize using a callback.
 	 * 
-	 * @param {string} componentName - The name of the component to register.
-	 * @param {function} [callback] - An optional function to execute, that configures the newly created {@link TableType}. It takes a single parameter: the newly created {@link TableType}
+	 * @param {string | TableType} nameOrTableType - The name of the component to register, or a {@link TableType} object.
+	 * @param {function} [callback] - An optional function to execute, that configures the newly created {@link TableType}. It takes a single parameter: the newly created {@link TableType}, and should return the transformed table type.
 	 * @returns {this} - For chaining.
 	 */
-	registerTableType(componentName, callback){
-		const tableType = new TableType(componentName);
+	registerTableType(nameOrTableType, callback){
+		const tableType = nameOrTableType instanceof TableType ? nameOrTableType : new TableType(nameOrTableType);
+		const transformedTableType = (callback && typeof callback === 'function') ? callback(tableType) || tableType : tableType;
 
-		this.tableTypes[componentName] = tableType;
-
-		if (callback && typeof callback === 'function'){
-			callback(tableType);
-		}
-
-		if (this.vueConstructor){
-			this.installTableType(componentName, tableType, this.vueConstructor);
+		const name = transformedTableType.id;
+		this.tableTypes[name] = transformedTableType;
+		if (this.vueInstance){
+			this.installTableType(name);
 		}
 
 		return this;
 	}
 
 	/**
-	 * Declares global components exported by vuejs-datatable, & load configs.
-	 * You should add table types to the {@link table_types} list before this function is called.
+	 * Creates a new table type with a specified prefix, that you can customize using a callback.
 	 * 
-	 * @param {Vue} Vue - The global Vue instance.
+	 * @param {string | TableType} nameOrTableType - The name of the component to register, or a {@link TableType} object.
+	 * @returns {this} - For chaining.
+	 */
+	deregisterTableType(nameOrTableType){
+		const name = nameOrTableType instanceof TableType ? nameOrTableType.id : nameOrTableType;
+
+		if (this.vueInstance){
+			this.uninstallTableType(name);
+		}
+		delete this.tableTypes[name];
+		
+		return this;
+	}
+
+	/**
+	 * Declares global components exported by vuejs-datatable, & load configs.
+	 * 
+	 * @param {VueConstructor} Vue - The Vue instance to configure.
 	 * @returns {void}
 	 */
 	install(Vue){
+		this.vueInstance = Vue;
 		Vue.prototype.$datatables = {};
 
 		Vue.component(`${ DEFAULT_DATATABLE }-cell`, VueDatatableCell);
 		Vue.component(`${ DEFAULT_DATATABLE }-header`, VueDatatableHeader);
 		Vue.component(`${ DEFAULT_DATATABLE }-button`, VueDatatablePagerButton);
-
-		if (this.tableTypes.hasOwnProperty(DEFAULT_DATATABLE)){
-			this._useDefaultType = true;
-		} else {
-			this.registerTableType(DEFAULT_DATATABLE, tableType => {
-				tableType.mergeSettings(this.defaultTableSettings.properties);
-			});
-		}
 		
-		for (const i in this.tableTypes){
-			if (this._useDefaultType || i !== DEFAULT_DATATABLE){
-				this.installTableType(this.tableTypes[i].getId(), this.tableTypes[i], Vue);
-			}
+		for (const type of Object.values(this.tableTypes)){
+			this.installTableType(type.id);
 		}
-		
-		this.vueConstructor = Vue;
 	}
 
 	/**
 	 * Declares a pair of components (a Datatable & a Datatable-Pager) sharing a config.
 	 * 
+	 * @private
 	 * @param {string} id - The base name of the datatable type.
 	 * @param {TableType} tableType - The configuration object that describes both datatable & the related pager.
-	 * @param {Vue} Vue - The global Vue instance.
-	 * @returns {void}
+	 * @returns {this} - For chaining.
 	 */
-	installTableType(id, tableType, Vue){
-		Vue.component(id, tableType.getTableDefinition());
-		Vue.component(`${ id }-pager`, tableType.getPagerDefinition());
+	installTableType(id){
+		const tableType = this.tableTypes[id];
+		const tableDef = tableType.getTableDefinition();
+		this.vueInstance.component(tableDef.name, tableDef);
+		const pagerDef = tableType.getPagerDefinition();
+		this.vueInstance.component(pagerDef.name, pagerDef);
+		return this;
+	}
+
+	/**
+	 * Remove a table type definition from vue (the datatable & its associated pager).
+	 * This should be used carefully, because Vue won't be able to instanciate new instances of this table type.
+	 * 
+	 * @private
+	 * @param {string} id - The base name of the datatable type to forget.
+	 * @returns {this} - For chaining.
+	 */
+	uninstallTableType(id){
+		const tableType = this.tableTypes[id];
+		const tableDef = tableType.getTableDefinition();
+		delete this.vueInstance.options.components[tableDef.name];
+		const pagerDef = tableType.getPagerDefinition();
+		delete this.vueInstance.options.components[pagerDef.name];
+		
+		return this;
 	}
 }
 
--- src/classes/factory.spec.ts
+++ src/classes/factory.spec.ts
@@ -1,64 +1,72 @@
-import Factory from './factory.js';
+jest.mock('./table-type');
+import TableType, { getTableDefinition, getPagerDefinition } from './table-type';
+import Factory from './factory';
 import { createLocalVue } from '@vue/test-utils';
 
 beforeEach(() => {
 	jest.clearAllMocks();
 });
-it('is initialized with empty types', () => {
+it('is initialized with only the default table type registered', () => {
+	jest.spyOn(Factory.prototype, 'registerTableType');
 	const factory = new Factory();
 
-	expect(factory.tableTypes instanceof Object).toBe(true);
-	expect(Object.keys(factory.tableTypes)).toHaveLength(0);
+	expect(factory.registerTableType).toHaveBeenCalledTimes(1);
+	expect(factory.registerTableType).toHaveBeenCalledWith(factory.defaultTableType);
 });
 
-it('is initialized using default datatable type', () => {
-	const factory = new Factory();
-
-	expect(factory.useDefaultType()).toBe(true);
-});
-
-it('can disable default datatable type', () => {
-	const factory = new Factory();
-
-	expect(factory.useDefaultType()).toBe(true);
-
-	factory.useDefaultType(false);
-
-	expect(factory.useDefaultType()).toBe(false);
-});
-
-describe('Registration', () => {
-	it('can register a new datatable type', () => {
+describe('Registration & installation', () => {
+	it.each`
+useTableType  | transform | transformReturn | afterInstall
+${false}      | ${false}  | ${false}        | ${false}
+${false}      | ${false}  | ${false}        | ${true}
+${false}      | ${true}   | ${false}        | ${false}
+${false}      | ${true}   | ${false}        | ${true}
+${false}      | ${true}   | ${true}         | ${false}
+${false}      | ${true}   | ${true}         | ${true}
+${true}       | ${false}  | ${false}        | ${false}
+${true}       | ${false}  | ${false}        | ${true}
+${true}       | ${true}   | ${false}        | ${false}
+${true}       | ${true}   | ${false}        | ${true}
+${true}       | ${true}   | ${true}         | ${false}
+${true}       | ${true}   | ${true}         | ${true}`(
+	'Use TableType "$useTableType", transform: "$transform" that returns: "$transformReturn", afterInstall: "$afterInstall"',
+	({ useTableType, transform, transformReturn, afterInstall }) => {
 		const factory = new Factory();
 
-		expect(Object.keys(factory.tableTypes)).toHaveLength(0);
+		const name = `${useTableType}-${transform}-${transformReturn}-${afterInstall}`;
 
-		factory.registerTableType('testtable');
+		let sourceTableType: TableType;
+		if (useTableType) {
+			sourceTableType = new TableType(name);
+		}
 
-		expect(Object.keys(factory.tableTypes)).toHaveLength(1);
-		expect(factory.tableTypes).toHaveProperty('testtable');
-	});
+		let tableTypeSave: TableType;
+		let mockTransform: jest.Mock<any, [any]>;
+		if (transform) {
+			mockTransform = transformReturn ?
+				jest.fn(tableType => tableTypeSave = Object.assign(Object.create(Object.getPrototypeOf(tableType)), tableType, { id: tableType.id })) :
+				jest.fn(tableType => { tableTypeSave = tableType; });
+		}
 
-	it('registration passes callback that can customize settings', () => {
-		const factory = new Factory();
-		const registerCb = jest.fn();
+		let mockInstall: jest.Mock<any, [any]>;
+		if (afterInstall) {
+			mockInstall = jest.spyOn(factory, 'installTableType').mockImplementation();
+			const localVue = createLocalVue();
+			factory.install(localVue);
+		}
 
-		factory.registerTableType('testtable', registerCb);
-		expect(registerCb).toHaveBeenCalledTimes(1);
-		expect(registerCb).toHaveBeenCalledWith(factory.tableTypes.testtable);
-	});
-});
+		factory.registerTableType(sourceTableType || name, mockTransform);
 
-describe('Installation', () => {
-	it('Can be installed into Vue', () => {
-		const factory = new Factory();
-
-		expect(typeof factory.install).toBe('function');
-		jest.spyOn(factory, 'installTableType').mockImplementation();
-		const localVue = createLocalVue();
-		factory.install(localVue as any);
-		expect(factory.installTableType).toHaveBeenCalledTimes(1);
-		expect(factory.installTableType).toHaveBeenCalledWith('datatable', factory.tableTypes.datatable, localVue);
+		expect(factory.tableTypes).toHaveProperty(name);
+		expect(factory.tableTypes[name]).toBeInstanceOf(TableType);
+		expect(factory.tableTypes[name].id).toEqual(name);
+		if (transform) {
+			expect(mockTransform).toHaveBeenCalledTimes(1);
+		}
+		if (afterInstall) {
+			expect(mockInstall).toHaveBeenCalledTimes(2);
+			expect(mockInstall).toHaveBeenNthCalledWith(2, name);
+		}
 	});
 
 	it('Queue registration while not installed', () => {
@@ -71,31 +79,114 @@ describe('Installation', () => {
 		expect(factory.installTableType).toHaveBeenCalledTimes(0);
 
 		const localVue = createLocalVue();
-		factory.install(localVue as any);
+		factory.install(localVue);
 
 		expect(factory.installTableType).toHaveBeenCalledTimes(3);
-		expect(factory.installTableType).toHaveBeenCalledWith('datatable', factory.tableTypes.datatable, localVue);
-		expect(factory.installTableType).toHaveBeenCalledWith('foo', factory.tableTypes.foo, localVue);
-		expect(factory.installTableType).toHaveBeenCalledWith('bar', factory.tableTypes.bar, localVue);
+		expect(factory.installTableType).toHaveBeenCalledWith('datatable');
+		expect(factory.installTableType).toHaveBeenCalledWith('foo');
+		expect(factory.installTableType).toHaveBeenCalledWith('bar');
 	});
+	it('Check installation', () => {
+		const factory = new Factory();
 
-	it('Registrer immediately once installed', () => {
+		const tt = new TableType('foo');
+		factory.tableTypes.foo = tt;
+		factory.vueInstance = createLocalVue();
+		jest.spyOn(factory.vueInstance, 'component').mockImplementation();
+		expect(factory.installTableType('foo')).toEqual(factory);
+
+		expect(factory.vueInstance.component).toHaveBeenCalledTimes(2);
+		expect(getTableDefinition).toHaveBeenCalledTimes(1);
+		expect(getPagerDefinition).toHaveBeenCalledTimes(1);
+		expect(factory.vueInstance.component).toHaveBeenNthCalledWith(1, 'foo', tt.getTableDefinition());
+		expect(factory.vueInstance.component).toHaveBeenNthCalledWith(2, 'foo-pager', tt.getPagerDefinition());
+	});
+});
+describe('Deregistration & uninstallation', () => {
+	it.each`
+useTableType  | afterInstall
+${false}      | ${false}
+${false}      | ${true}
+${true}       | ${false}
+${true}       | ${true}`(
+	'Use TableType "$useTableType", afterInstall: "$afterInstall"',
+	({ useTableType, afterInstall }) => {
+		const factory = new Factory();
+
+		const name = `datatable`;
+
+		let sourceTableType: TableType;
+		if (useTableType) {
+			sourceTableType = factory.tableTypes[name];
+		}
+
+		let mockUninstall: jest.Mock<any, [any]>;
+		if (afterInstall) {
+			mockUninstall = jest.spyOn(factory, 'uninstallTableType').mockImplementation();
+			const localVue = createLocalVue();
+			factory.install(localVue);
+		}
+
+		expect(factory.tableTypes).toHaveProperty(name);
+		factory.deregisterTableType(sourceTableType || name);
+
+		expect(factory.tableTypes).not.toHaveProperty(name);
+		if (afterInstall) {
+			expect(mockUninstall).toHaveBeenCalledTimes(1);
+			expect(mockUninstall).toHaveBeenCalledWith(name);
+		}
+	});
+	it('Deregistered table won\'t be installed on factory installation', () => {
 		const factory = new Factory();
 
 		jest.spyOn(factory, 'installTableType').mockImplementation();
+		jest.spyOn(factory, 'uninstallTableType').mockImplementation();
+
+		factory.deregisterTableType('datatable');
+		expect(factory.installTableType).toHaveBeenCalledTimes(0);
+		expect(factory.uninstallTableType).toHaveBeenCalledTimes(0);
 
 		const localVue = createLocalVue();
-		factory.install(localVue as any);
+		jest.spyOn(localVue, 'component').mockImplementation();
+		factory.install(localVue);
 
-		expect(factory.installTableType).toHaveBeenCalledTimes(1);
-		expect(factory.installTableType).toHaveBeenLastCalledWith('datatable', factory.tableTypes.datatable, localVue);
-
-		factory.registerTableType('foo');
-		expect(factory.installTableType).toHaveBeenCalledTimes(2);
-		expect(factory.installTableType).toHaveBeenLastCalledWith('foo', factory.tableTypes.foo, localVue);
-
-		factory.registerTableType('bar');
-		expect(factory.installTableType).toHaveBeenCalledTimes(3);
-		expect(factory.installTableType).toHaveBeenLastCalledWith('bar', factory.tableTypes.bar, localVue);
+		expect(factory.vueInstance.component).toHaveBeenCalledTimes(3);
+		expect(getTableDefinition).toHaveBeenCalledTimes(0);
+		expect(getPagerDefinition).toHaveBeenCalledTimes(0);
+		expect(factory.installTableType).toHaveBeenCalledTimes(0);
+		expect(factory.uninstallTableType).toHaveBeenCalledTimes(0);
 	});
+	it('Check uninstallation', () => {
+		const factory = new Factory();
+
+		const tt = new TableType('foo');
+		factory.tableTypes.foo = tt;
+		factory.vueInstance = createLocalVue();
+		expect(factory.installTableType('foo')).toEqual(factory);
+
+		expect(getTableDefinition).toHaveBeenCalledTimes(1);
+		expect(getPagerDefinition).toHaveBeenCalledTimes(1);
+		expect(Object.keys(factory.vueInstance.options.components)).toHaveLength(2);
+		expect(factory.uninstallTableType('foo')).toEqual(factory);
+		expect(getTableDefinition).toHaveBeenCalledTimes(2);
+		expect(getPagerDefinition).toHaveBeenCalledTimes(2);
+		expect(Object.keys(factory.vueInstance.options.components)).toHaveLength(0);
+	});
+});
+
+it('Get table type', () => {
+	const factory = new Factory();
+	expect(factory.getTableType()).toEqual(factory.tableTypes.datatable);
+	expect(factory.getTableType('datatable')).toEqual(factory.tableTypes.datatable);
+	expect(factory.getTableType('missing')).toBeUndefined();
+});
+describe('Use default type function', () => {
+	const factory = new Factory();
+
+	expect(factory.useDefaultType()).toEqual(true);
+	expect(factory.tableTypes).toHaveProperty('datatable', factory.defaultTableType);
+	expect(factory.useDefaultType(false)).toEqual(factory);
+	expect(factory.tableTypes).not.toHaveProperty('datatable');
+	expect(factory.useDefaultType(true)).toEqual(factory);
+	expect(factory.tableTypes).toHaveProperty('datatable', factory.defaultTableType);
 });
