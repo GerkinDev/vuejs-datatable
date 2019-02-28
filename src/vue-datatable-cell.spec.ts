4720fe9a2f8e6d8960e070eb65a6eef1958b2cc3
Alexandre Germain <agermain@ithoughts.io>
2019-02-28 15:02:16 +0100

    test: 💍 Fixed `DatatableCell` tests, remove unused

--- src/vue-datatable-cell.spec.ts
+++ src/vue-datatable-cell.spec.ts
@@ -1,154 +1,56 @@
+jest.mock('./classes/column');
 import { createLocalVue, mount } from '@vue/test-utils';
-import Column from './classes/column.js';
+import Column, { getRepresentation } from './classes/column.js';
 import DatatableCell from './vue-datatable-cell.vue';
 
 const localVue = createLocalVue();
-const Ctor = localVue.extend(DatatableCell);
 
-localVue.component('test-component', {
-	props:    [ 'row' ],
-	render: c => c('span', 'Test Component')
-});
+const TestComponent = {
+	props:  [ 'row', 'column' ],
+	render: (c) => c('')
+};
+localVue.component('test-component', TestComponent);
 
-it('displays field', () => {
-	const wrapper = mount(Ctor, {
-		propsData: {
-			column: new Column({
-				label: 'ID',
-				field: 'user.id'
-			}),
-			row: {
-				user: {
-					id: 7
-				}
-			}
-		},
+it('displays column representation', () => {
+	getRepresentation.mockReturnValue('retRepresentation');
+	const col = new Column({});
+	const row = {};
+	const wrapper = mount(DatatableCell, {
+		propsData: { column: col, row },
 		localVue
 	});
 
-	expect(wrapper.element.textContent.trim()).toBe('7');
-});
-
-it('displays representation', () => {
-	const wrapper = mount(Ctor, {
-		propsData: {
-			column: new Column({
-				label:         'ID',
-				representedAs: row => `${ row.user.firstName } ${ row.user.lastName }`
-			}),
-			row: {
-				user: {
-					firstName: 'John',
-					lastName:  'Doe'
-				}
-			}
-		},
-		localVue
-	});
-
-	expect(wrapper.element.textContent.trim()).toBe('John Doe');
-});
-
-it('gives representation presidence over field', () => {
-	const wrapper = mount(Ctor, {
-		propsData: {
-			column: new Column({
-				label:         'ID',
-				field:         'id',
-				representedAs: row => `${ row.user.firstName } ${ row.user.lastName }`
-			}),
-			row: {
-				id:   5,
-				user: {
-					firstName: 'Jack',
-					lastName:  'Doe'
-				}
-			}
-		},
-		localVue
-	});
-
-	expect(wrapper.element.textContent.trim()).toBe('Jack Doe');
+	expect(wrapper.element.textContent.trim()).toBe('retRepresentation');
+	expect(getRepresentation).toHaveBeenCalledTimes(1);
+	expect(getRepresentation).toHaveBeenCalledWith(row);
 });
 
 it('displays component', () => {
-	const wrapper = mount(Ctor, {
-		propsData: {
-			column: new Column({
-				label:     'ID',
-				component: 'test-component'
-			}),
-			row: {}
-		},
+	const col = new Column({ component: 'test-component' });
+	const row = {};
+	const wrapper = mount(DatatableCell, {
+		propsData: { column: col, row: row },
 		localVue
 	});
 
-	expect(wrapper.element.textContent.trim()).toBe('Test Component');
+	expect(wrapper.contains(TestComponent)).toBe(true);
+	const mountedTestComponent = wrapper.find(TestComponent);
+	expect(mountedTestComponent.props().row).toBe(row);
+	expect(mountedTestComponent.props().column).toBe(col);
 });
-
-it('gives component presidence', () => {
-	const wrapper = mount(Ctor, {
-		propsData: {
-			column: new Column({
-				label:         'ID',
-				field:         'id',
-				representedAs: row => row.user.firstName,
-				component:     'test-component'
-			}),
-			row: {
-				id:   6,
-				user: {
-					firstName: 'Jimmy'
-				}
-			}
-		}
+it('can change text alignment', () => {
+	const wrapperL = mount(DatatableCell, {
+		propsData: { column: new Column({ align: 'left' }), row: {} }
 	});
+	expect(wrapperL.element.style.textAlign).toBe('left');
 
-	expect(wrapper.element.textContent.trim()).toBe('Test Component');
-});
-
-it('will show correct alignment', () => {
-	const wrapper1 = mount(Ctor, {
-		propsData: {
-			column: new Column({
-				label: 'id',
-				field: 'id'
-			}),
-			row: {
-				id: 7
-			}
-		}
+	const wrapperC = mount(DatatableCell, {
+		propsData: { column: new Column({ align: 'center' }), row: {} }
 	});
-
-	expect(wrapper1.element.style.textAlign).toBe('left');
-
-	const wrapperC = mount(Ctor, {
-		propsData: {
-			column: new Column({
-				label: 'id',
-				field: 'id',
-				align: 'center'
-			}),
-			row: {
-				id: 7
-			}
-		}
-	});
-
 	expect(wrapperC.element.style.textAlign).toBe('center');
 
-	const wrapperR = mount(Ctor, {
-		propsData: {
-			column: new Column({
-				label: 'id',
-				field: 'id',
-				align: 'right'
-			}),
-			row: {
-				id: 7
-			}
-		}
+	const wrapperR = mount(DatatableCell, {
+		propsData: { column: new Column({ align: 'right' }), row: {} }
 	});
-
 	expect(wrapperR.element.style.textAlign).toBe('right');
 });
